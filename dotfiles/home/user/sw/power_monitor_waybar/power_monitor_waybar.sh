a874f3d0b7eb45452abd213237de1d95011a58db
strg-v <vincentvibr@gmail.com>
2025-06-07 16:49:39 +0200

    adding bluetooth to waybar, adding remaining battery time to power bash script

--- dotfiles/home/user/sw/power_monitor_waybar/power_monitor_waybar.sh
+++ dotfiles/home/user/sw/power_monitor_waybar/power_monitor_waybar.sh
@@ -2,11 +2,17 @@
 
 #   Author: strg-v
 #   Date: 2025-06-04
+#   
+#   Requires upower for remaining battery in tooltip field
+#
+#   argument 1: path to /sys/class/power_supply/[...] device with current_now and voltage_now files
+#   optional argument 2: "org-path", to upower device stating remaining battery duration
 #
 #   Usage example:
-#   ./power_monitor_waybar /sys/class/power_supply/BAT0
+#   ./power_monitor_waybar.sh /sys/class/power_supply/BAT0 /org/freedesktop/UPower/devices/battery_BAT0
 
 BASE_PATH="$1"
+REMAINING_BATTERY_PATH="$2"
 
 CURRENT_UA=$(cat ${BASE_PATH}/current_now)
 VOLTAGE_UV=$(cat ${BASE_PATH}/voltage_now)
@@ -24,9 +30,41 @@ else
 MULTIPLIER="-"
 fi
 
-
 POWER_UW=$((CURRENT_UA*VOLTAGE_UV))
 
 POWER_W=$(awk "BEGIN {printf \"%.1f\", $POWER_UW / 1000000000000}")
 
-echo { \"text\": \"${MULTIPLIER}${POWER_W}W\" }
+REMAINING_BATTERY=""
+
+if [ -z $REMAINING_BATTERY_PATH ]; then
+
+echo { \"text\": \"${MULTIPLIER}${POWER_W}W\", \"tooltip\": \"${REMAINING_BATTERY}\" }
+exit 0
+
+else
+
+LINE=$(upower -i $REMAINING_BATTERY_PATH | grep "time to empty")
+
+LINE=${LINE//"time to empty:"}
+LINE=${LINE//" "}
+LINE=${LINE//","/"."}
+
+UNIT=""
+
+if [[ $LINE =~ "hours" ]]; then
+LINE=${LINE//"hours"}
+UNIT="h"
+REMAINING_BATTERY=$LINE$UNIT
+
+elif [[ $LINE =~ "minutes" ]]; then
+LINE=${LINE//"minutes"}
+UNIT="min"
+REMAINING_BATTERY="<span color=\\\"#FF9F1C\\\">"$LINE$UNIT"</span>"
+fi
+
+
+echo { \"text\": \"${MULTIPLIER}${POWER_W}W\", \"alt\": \"${REMAINING_BATTERY}\" }
+exit 0
+
+fi
+
